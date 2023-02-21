1dc497e8d4d0eeb5cb150ca3b205ef444368c5f2
Adel-Al-Qosi <Adel.Al.Qosi@gmail.com>
2023-02-21 18:37:26 +0300

    finish practice

--- classes/person.js
+++ classes/person.js
@@ -1,5 +1,34 @@
 // Your code here
+class Person {
+  constructor(firstName, lastName, age) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.age = age;
+  }
 
+  introduce() {
+    console.log(
+      `Hi, I'm ${this.firstName} ${this.lastName}, and I'm ${this.age} years old.`
+    );
+  }
+
+  static introducePeople(input) {
+    if (!Array.isArray(input)) {
+      throw new Error("introducePeople only takes an array as an argument.");
+    } else if (
+      input.reduce((boolean, element) => {
+        if (element instanceof Person) return boolean;
+        return true;
+      }, false)
+    ) {
+      throw new Error("All items in array must be Person class instances.");
+    } else {
+      input.map((element) => {
+        element.introduce();
+      });
+    }
+  }
+}
 /****************************************************************************/
 /******************* DO NOT EDIT CODE BELOW THIS LINE ***********************/
 
